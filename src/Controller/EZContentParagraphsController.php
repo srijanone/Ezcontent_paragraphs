3edeab485567df9a204641ef0e330f0ea31c5c0f
Abhinesh Dhiman <abhinesh.dhiman@materialplus.io>
2024-09-17 07:50:41 +0530

    AddedEzContentParagraphModule

---
+++ src/Controller/EZContentParagraphsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\ezcontent_paragraphs\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Defines a route controller.
+ */
+class EZContentParagraphsController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build($paragraph, $view_mode = 'default') {
+    $content = $this->entityTypeManager()
+      ->getViewBuilder('paragraph')
+      ->view($paragraph, $view_mode);
+    return new Response(\Drupal::service('renderer')->render($content));
+  }
+
+}
