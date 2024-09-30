3df4790da9063a63d4dc95eb6575633d3e5a3d94
Abhinesh Dhiman <abhinesh.dhiman@materialplus.io>
2024-09-30 13:22:26 +0530

    AddedCacheMetaTagsForFieldFormatter

--- src/Plugin/Field/FieldFormatter/EmbedFieldFormatter.php
+++ src/Plugin/Field/FieldFormatter/EmbedFieldFormatter.php
@@ -4,6 +4,8 @@ namespace Drupal\ezcontent_paragraphs\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Render\RendererInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'embed' formatter.
@@ -26,12 +28,31 @@ class EmbedFieldFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode = NULL) {
     $elements = [];
+    // Collect cache metadata.
+    $cache_tags = [];
+    $cache_contexts = [];
+    $cache_max_age = [];
+
     foreach ($items as $delta => $item) {
       $elements[$delta] = [
         '#type' => 'inline_template',
         '#template' => $item->value,
       ];
+
+      // Add cache metadata based on each item.
+      // For example, if the content or language changes, this should be reflected.
+      $cache_tags = array_merge($cache_tags, $item->getCacheTags());
+      $cache_contexts = array_merge($cache_contexts, $item->getCacheContexts());
+      $cache_max_age = Cache::mergeMaxAges($cache_max_age, $item->getCacheMaxAge());
     }
+
+    // Attach cache metadata to the render array.
+    $elements['#cache'] = [
+      'tags' => $cache_tags,
+      'contexts' => $cache_contexts,
+      'max-age' => $cache_max_age,
+    ];
+
     return $elements;
   }
 
